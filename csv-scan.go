9dd4623e4ed83840756e88c2496269b56594fb95
Andrew Hase <oldm8reggie@gmail.com>
2023-12-18 18:49:32 +0100

    Struggle to get a workging file change. Works in Windows

--- csv-scan.go
+++ csv-scan.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
@@ -10,12 +11,13 @@ import (
 func analyseReplace(csvPath string) {
 
 	// seperators := []string{",", ";"}
+	//TODO Build a bigger list of known deliminators
 	seperators := []rune{',', ';'} //use rune. https://go.dev/blog/strings bufio streams in bytes
 
 	csv, err := os.Open(csvPath)
 	if err != nil {
 		fmt.Println(err)
-		return
+		// return err.Error()
 	}
 
 	defer csv.Close()
@@ -25,18 +27,67 @@ func analyseReplace(csvPath string) {
 	for _, delimiter := range seperators {
 
 		if strings.Contains(line, string(delimiter)) {
-			fmt.Println("Here")
-			fmt.Println(delimiter)
+			// fmt.Println("Here")
+			fmt.Println(string(delimiter))
+			// return string(delimiter)
+			//add sep=(delimiter)
+			//Reset to begginging
+			csv.Seek(0, 0)
+			seperatorLine := fmt.Sprintf("sep=%s\n", string(delimiter))
+			tempFile, err := os.CreateTemp("", "tempCSVFile")
+			//Error to create temp file
+			if err != nil {
+				fmt.Println("Error creating temp file:", err)
+				return
+			}
+			defer tempFile.Close()
+			//No longer want read open
+			// csv.Close()
+			_, _ = fmt.Fprint(tempFile, seperatorLine)
+			_, err = io.Copy(tempFile, csv)
+			if err != nil {
+				panic(err)
+			}
+			csv.Close()
+			tempFile.Close()
+
+			//! DANGER
+			//REMOVE THE ORIGINAL FILE AND COPY OUR TEMP FILE
+			err = os.Remove(csvPath)
+			if err != nil {
+				panic(err)
+			}
+			// Rename and copy to replace original file
+			err = os.Rename(tempFile.Name(), csvPath)
+			if err != nil {
+				panic(err) //We done messed up now
+			}
+
+			// -------------------
+			//stream in new content followed by the rest of the horse
+			// csvEdit, err := os.OpenFile(csvPath, os.O_APPEND, 0600)
+			// if err != nil {
+			// 	panic(err)
+			// }
+			// defer csvEdit.Close()
+			// _, err = csvEdit.Seek(0, 0)
+			// if err != nil {
+			// 	panic(err)
+			// }
+			// _, err = csvEdit.WriteString(seperatorLine)
+			// if err != nil {
+			// 	panic(err)
+			// }
+			// csvEdit.Close()
+
 		}
 
 	}
-	// for scanner.Scan() {
-	// 	line := scanner.Text()
-	// 	fmt.Println("Line: ", line)
-	// }
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Scanner error:", err)
+		// return err.Error()
 	}
+	// return ""
 
 }
 func listFiles(dir string) {
@@ -72,7 +123,11 @@ func listFiles(dir string) {
 func main() {
 
 	// fmt.Println("Happy")
-	listFiles("/home/drew/Projects") //linux
-	// listFiles("%USER%\\Documents\\Projects\\goprojects") //windows
+	// listFiles("/home/drew/Projects") //linux
+	// userHomeDir, err := os.UserHomeDir()
+	// if err != nil {
+	// 	// return err
+	// }
+	listFiles("C:\\Users\\Drew\\Documents\\Projects\\goprojects") //windows
 
 }
